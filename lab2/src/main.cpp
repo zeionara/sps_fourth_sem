f66bcafc11bd8a94b685a269e8ed6a9e55583873
ZeioNara <zeionara@gmail.com>
2017-03-05 18:11:44 +0300

    Added sorting test

--- lab2/src/main.cpp
+++ lab2/src/main.cpp
@@ -6,6 +6,7 @@
 #include <windows.h>
 #include <time.h>
 using namespace std;
+
 #define ID_MAX_LENGTH 32
 
 typedef struct _treeNode{
@@ -15,15 +16,24 @@ typedef struct _treeNode{
 
 typedef struct _treeNodeB{
   struct _treeNodeB *left, *right;
-  char* identifier;
+  DWORD identifier;
 } treeNodeB, *treeNodeBPointer, **treeNodeBBasePointer;
 
 #define NODE_SIZE sizeof(treeNode)
+#define NODEB_SIZE sizeof(treeNodeB)
 #define NODE_HEAP_ISIZE 0x8000
 #define ID_SIZE ID_MAX_LENGTH * sizeof(char)
 
+char* getFullPointer(DWORD offset, char* base){
+  return (offset + base);
+}
+
 int KeyCompare(const char* identifier1, const char* identifier2){
   return strcmp(identifier1, identifier2);
+}
+
+int KeyCompare(DWORD identifier1,DWORD identifier2, char* base){
+  return strcmp(base+identifier1, base+identifier2);
 }
 
 BOOL InsertTree(treeNodeBasePointer rootBasePointer, treeNodePointer nodePointer){
@@ -38,41 +48,16 @@ BOOL InsertTree(treeNodeBasePointer rootBasePointer, treeNodePointer nodePointer
   }
 }
 
-char* getFullPointer(char* offset, char* base){
-  return (char*)((int)offset + base);
-  //return (char*)((DWORD)nodePointer->identifier + (DWORD)fileContent)
-}
-
-int KeyCompare(char* identifier1,char* identifier2, char* base){
-  return strcmp(getFullPointer(identifier1,base), getFullPointer(identifier2,base));
-}
-
-BOOL InsertTree(treeNodeBBasePointer rootBasePointer, treeNodeBPointer nodePointer, char* fileContent){
+BOOL InsertTree(treeNodeBBasePointer rootBasePointer, treeNodeBPointer nodePointer, char* base){
   if (*rootBasePointer == NULL){
     *rootBasePointer = nodePointer;
     return TRUE;
   }
-  if (KeyCompare(nodePointer->identifier,(*rootBasePointer)->identifier,fileContent) < 0){
-    InsertTree(&((*rootBasePointer)->left),nodePointer,fileContent);
+  if (KeyCompare(nodePointer->identifier,(*rootBasePointer)->identifier,base) < 0){
+    InsertTree(&((*rootBasePointer)->left),nodePointer,base);
   } else {
-    InsertTree(&((*rootBasePointer)->right),nodePointer,fileContent);
+    InsertTree(&((*rootBasePointer)->right),nodePointer,base);
   }
-}
-
-treeNodePointer fillTreeHeap(const char* fileName, HANDLE hNode){
-    treeNodePointer rootPointer = NULL, nodePointer;
-    char identifier[ID_MAX_LENGTH];
-    ifstream identifiers(fileName);
-    if (identifiers == NULL){
-      return NULL;
-    }
-    while (identifiers){
-      identifiers.getline(identifier, ID_MAX_LENGTH);
-      nodePointer = (treeNodePointer)HeapAlloc(hNode, HEAP_ZERO_MEMORY, NODE_SIZE);
-      strncpy(nodePointer->identifier,identifier,ID_SIZE);
-      InsertTree(&rootPointer, nodePointer);
-    }
-    return rootPointer;
 }
 
 DWORD find(const char* string, char rc, DWORD start, DWORD maxindex){
@@ -92,16 +77,25 @@ DWORD find(const char* string, char rc, DWORD start, DWORD maxindex){
   return 0;
 }
 
+treeNodePointer fillTreeHeap(const char* fileName, HANDLE hNode){
+    treeNodePointer rootPointer = NULL, nodePointer;
+    char identifier[ID_MAX_LENGTH];
+    ifstream identifiers(fileName);
+    if (identifiers == NULL){
+      return NULL;
+    }
+    while (identifiers){
+      identifiers.getline(identifier, ID_MAX_LENGTH);
+      nodePointer = (treeNodePointer)HeapAlloc(hNode, HEAP_ZERO_MEMORY, NODE_SIZE);
+      strncpy(nodePointer->identifier,identifier,ID_SIZE);
+      InsertTree(&rootPointer, nodePointer);
+    }
+    return rootPointer;
+}
+
 treeNodePointer fillTreeMMF(const char* fileName, HANDLE hNode, LPVOID mappedFile,DWORD fileSize){
     treeNodePointer rootPointer = NULL, nodePointer;
-    //char identifier[ID_MAX_LENGTH];
-    //ifstream identifiers(fileName);
-    //if (identifiers == NULL){
-    //  return NULL;
-    //}
-
     char* fileContent = (char*)mappedFile;
-
     DWORD old_pos = 0;
     DWORD pos = -1;
     char identifier[32];
@@ -115,29 +109,15 @@ treeNodePointer fillTreeMMF(const char* fileName, HANDLE hNode, LPVOID mappedFil
       nodePointer = (treeNodePointer)HeapAlloc(hNode, HEAP_ZERO_MEMORY, NODE_SIZE);
       fileContent[pos] = '\0';
       strncpy(nodePointer->identifier,fileContent+old_pos,ID_SIZE);
-      //cout << "We have new identifier : " << nodePointer->identifier << endl;
       fileContent[pos] = '\n';
       InsertTree(&rootPointer, nodePointer);
       i++;
     }
-
-    /*while (identifiers){
-      identifiers.getline(identifier, ID_MAX_LENGTH);
-      nodePointer = (treeNodePointer)HeapAlloc(hNode, HEAP_ZERO_MEMORY, NODE_SIZE);
-      strncpy(nodePointer->identifier,identifier,ID_SIZE);
-      InsertTree(&rootPointer, nodePointer);
-    }*/
     return rootPointer;
 }
 
 treeNodeBPointer fillTreeBases(const char* fileName, HANDLE hNode, LPVOID mappedFile,DWORD fileSize){
     treeNodeBPointer rootPointer = NULL, nodePointer;
-    //char identifier[ID_MAX_LENGTH];
-    //ifstream identifiers(fileName);
-    //if (identifiers == NULL){
-    //  return NULL;
-    //}
-
     char* fileContent = (char*)mappedFile;
 
     DWORD old_pos = 0;
@@ -150,41 +130,13 @@ treeNodeBPointer fillTreeBases(const char* fileName, HANDLE hNode, LPVOID mapped
       if (pos == 0) {
         break;
       }
-      nodePointer = (treeNodeBPointer)HeapAlloc(hNode, HEAP_ZERO_MEMORY, NODE_SIZE);
+      nodePointer = (treeNodeBPointer)HeapAlloc(hNode, HEAP_ZERO_MEMORY, NODEB_SIZE);
       fileContent[pos] = '\0';
-      nodePointer->identifier = (char*)old_pos;
-      //cout << "We have new identifier : " << getFullPointer(nodePointer->identifier,fileContent) << endl;
-      //fileContent[pos] = '\n';
+      nodePointer->identifier = old_pos;
       InsertTree(&rootPointer, nodePointer, fileContent);
       i++;
     }
-
-    /*while (identifiers){
-      identifiers.getline(identifier, ID_MAX_LENGTH);
-      nodePointer = (treeNodePointer)HeapAlloc(hNode, HEAP_ZERO_MEMORY, NODE_SIZE);
-      strncpy(nodePointer->identifier,identifier,ID_SIZE);
-      InsertTree(&rootPointer, nodePointer);
-    }*/
     return rootPointer;
-}
-
-BOOL Scan(treeNodePointer nodePointer,int counter){
-  if (nodePointer == NULL){
-    cout << endl;
-    return true;
-  }
-  cout << nodePointer->identifier << endl;
-  for (int i = 0; i < counter; i++){
-    cout << "|  ";
-  }
-  cout << "|---Left subtree of " << nodePointer->identifier << " : ";
-  Scan(nodePointer->left,counter+2);
-  for (int i = 0; i < counter; i++){
-    cout << "|  ";
-  }
-  cout << "|---Right subtree of " << nodePointer->identifier << " : ";
-  Scan(nodePointer->right,counter+2);
-  return true;
 }
 
 int generateFile(int numberOfIdentifiers, const char* fileName, int minlength, int maxlength){
@@ -258,6 +210,7 @@ int showTableHead(){
 
 int main(int argc, char* argv[]){
   cout << "Test creating binary tree from the data in external file (time in seconds):" << endl;
+
   HANDLE hNode = NULL;
   treeNodePointer rootPointer;
   int numberOfIdentifiers = 0;
@@ -267,43 +220,42 @@ int main(int argc, char* argv[]){
   LPVOID mappedFile = NULL;
   DWORD FsLow, Result = 2;
   DWORD oop;
+  float tmp;
 
   if (argc != 2){
     cout << "Usage : heaps path/to/file" << endl;
   }
   showTableHead();
-  float tmp;
   for (int i = 1; i <= 10; i++){
+    numberOfIdentifiers = i*100000;
+    generateFile(numberOfIdentifiers,argv[1],3,32);
+
     //heap
     hNode = HeapCreate(HEAP_GENERATE_EXCEPTIONS | HEAP_NO_SERIALIZE, NODE_HEAP_ISIZE, 0);
-    numberOfIdentifiers = i*100000;
-    //hFile = CreateFile(argv[1], GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
 
-    generateFile(numberOfIdentifiers,argv[1],3,32);
     float fTimeStart = clock()/(float)CLOCKS_PER_SEC;
     rootPointer = fillTreeHeap(argv[1],hNode);
     float fTimeStop = clock()/(float)CLOCKS_PER_SEC;
-    //cout << "In heap tree containing " << numberOfIdentifiers << " elements was filled in " << fTimeStop - fTimeStart << " secs " << endl;
     tmp = fTimeStop - fTimeStart;
+
     if (hNode != NULL) HeapDestroy(hNode);
     hNode = NULL;
 
     //MMF
     hFile = CreateFile(argv[1], GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
-
     DWORD fileSize = GetFileSize(hFile,&oop);
+
     showCell((float)fileSize/1048576);
     showCell(tmp);
+
     hMap = CreateFileMapping(hFile, NULL, PAGE_READWRITE, 0, fileSize, NULL);
     hNode = HeapCreate(HEAP_GENERATE_EXCEPTIONS | HEAP_NO_SERIALIZE, NODE_HEAP_ISIZE, 0);
     mappedFile = MapViewOfFile(hMap,FILE_MAP_ALL_ACCESS,0,0,fileSize);
 
-    hNode = HeapCreate(HEAP_GENERATE_EXCEPTIONS | HEAP_NO_SERIALIZE, NODE_HEAP_ISIZE, 0);
-
     fTimeStart = clock()/(float)CLOCKS_PER_SEC;
     rootPointer = fillTreeMMF(argv[1],hNode,mappedFile,fileSize);
     fTimeStop = clock()/(float)CLOCKS_PER_SEC;
-    //cout << "In heap tree containing " << numberOfIdentifiers << " elements with using MMF was filled in " << fTimeStop - fTimeStart << " secs " << endl;
+
     showCell(fTimeStop - fTimeStart);
     UnmapViewOfFile(mappedFile);
     CloseHandle(hMap);
@@ -313,17 +265,14 @@ int main(int argc, char* argv[]){
 
     //bases
     hFile = CreateFile(argv[1], GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
-    fileSize = GetFileSize(hFile,&oop);
     hMap = CreateFileMapping(hFile, NULL, PAGE_READWRITE, 0, fileSize, NULL);
     hNode = HeapCreate(HEAP_GENERATE_EXCEPTIONS | HEAP_NO_SERIALIZE, NODE_HEAP_ISIZE, 0);
     mappedFile = MapViewOfFile(hMap,FILE_MAP_ALL_ACCESS,0,0,fileSize);
 
-    hNode = HeapCreate(HEAP_GENERATE_EXCEPTIONS | HEAP_NO_SERIALIZE, NODE_HEAP_ISIZE, 0);
-
     fTimeStart = clock()/(float)CLOCKS_PER_SEC;
-    treeNodeBPointer ootPointer = fillTreeBases(argv[1],hNode,mappedFile,fileSize);
+    treeNodeBPointer rootPointer = fillTreeBases(argv[1],hNode,mappedFile,fileSize);
     fTimeStop = clock()/(float)CLOCKS_PER_SEC;
-    //cout << "In heap tree containing " << numberOfIdentifiers << " elements with using MMF and based pointers was filled in " << fTimeStop - fTimeStart << " secs " << endl;
+
     showCell(fTimeStop - fTimeStart," |");
     UnmapViewOfFile(mappedFile);
     CloseHandle(hMap);
