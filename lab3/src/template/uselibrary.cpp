eb7090d1d6f0a30a05f17c23634728fbea46dd5d
ZeioNara <zeionara@gmail.com>
2017-03-13 00:37:55 +0300

    Works with a lot of crutches in last function

--- lab3/src/template/uselibrary.cpp
+++ lab3/src/template/uselibrary.cpp
@@ -12,6 +12,7 @@ int main(){
   VOID (*testfunctionprotype)(LPCTSTR);
   VOID (*sort)(LPDWORD);
   VOID (*strmask)();
+  VOID (*fpu)(LPCTSTR, LPCTSTR, LPCTSTR, LPCTSTR, LPVOID);
   if (hDll == NULL){
     cout << "Library is not loaded"<< endl;
     return 1;
@@ -24,7 +25,7 @@ int main(){
   }
   cout << "Proc found" << endl;
   testfunctionprotype = (VOID(*)(LPCTSTR))testfunction;
-  testfunctionprotype("Soon");
+  //testfunctionprotype("Soon");
 
   FARPROC firstlab = GetProcAddress(hDll,"FirstLab");
   if (firstlab == NULL){
@@ -45,5 +46,31 @@ int main(){
   cout << "Proc found" << endl;
   strmask = (VOID(*)())seclab;
   strmask();
+  string x0str = "1.1";
+  string xnstr = "2.5";
+  string step = "0.2";
+  string acc = "0.0001";
+
+  cout << "Please, type x0 : " << endl;
+  //cin >> x0str;
+  cout << "Please, type xn : " << endl;
+  //cin >> xnstr;
+  cout << "Please, type step : " << endl;
+  //cin >> step;
+  cout << "Please, type accuracy : " << endl;
+  //cin >> acc;
+
+  FARPROC sevlab = GetProcAddress(hDll,"SevLab");
+  if (sevlab == NULL){
+    cout << "Proc wasn't found" << endl;
+    return 1;
+  }
+  cout << "Proc found" << endl;
+  fpu = (VOID(*)(LPCTSTR, LPCTSTR, LPCTSTR, LPCTSTR, LPVOID))sevlab;
+   cout << "kk" << endl;
+  cout << &&rre << endl;
+  fpu("1.1", "2.5", "0.2", "0.0001", &&rre);
+
+  rre: cout << "oooo" << endl;
   FreeLibrary(hDll);
 }
